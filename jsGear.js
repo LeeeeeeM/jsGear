16c64f3e421da1544db6807fd5dc41e02bc654d5
evilemon <slm_1990@126.com>
2018-01-16 21:04:22 +0800

    complete

--- jsGear.js
+++ jsGear.js
@@ -1,116 +1,44 @@
 // 改变定时器的核心是改变系统时钟，通过改变时钟频率，自由定制时间队列，到达某个时刻执行函数，将异步执行转换为同步执行
-
-window.Gear = (function(global, rate) {
+(function(global) {
 	// 设置rate  [0-5]
 
-	function setRate(n) {
-		n = n - 2 // n [0, 5]整数
-		return Math.pow(10, n);
-	}
+	var __Date = global.Date;
 
-	var beginTime = new __Date()
-	var LastTime = new __Date()
-
-	var rate = 1
-
-	function hook() {
-		global.__setTimeout = global.setTimeout
-		global.__setInterval = global.setInterval
-		global.__clearTimeout = global.clearTimeout
-		global.__clearInterval = global.clearInterval
-
-		global.__set_rate__ = 1
-
-		global.setTimeout = setTimeout
-		global.setInterval = setInterval
-		global.clearTimeout = clearTimeout
-		global.clearInterval = clearInterval
-	}
-
-	function unhook() {
-
-	}
-
-	function setTimeout(fn, delay, arg) {
-		if (arg) {
-			arg = Array.prototype.slice.call(arguments, 2)
-		}
-		return addQue(fn, delay, arg, false)
-	}
-
-	function setInterval(fn, delay, arg) {
-		if (arg) {
-			arg = Array.prototype.slice.call(arguments, 2)
-		}
-		return addQue(fn, delay, arg, true)
-	}
-
-	function unhook() {
-		//
-	}
-
-
-	var tQueue = []
-
-	// function IndexedQueue = function() {
-	// 	this.Queue = []
-	// 	this.indexQueue = {
-
-	// 	}
-	// }
-	// IndexedQueue.prototype = {
-	// 	map: function(fn) {
-
-	// 	},
-	// 	push: function(item) {
-	// 		if (!item.name)
-	// 	},
-	// 	getByIndex: function(index) {
-
-	// 	},
-	// 	getByName: function(name) {
-
-	// 	},
-	// 	removeByIndex: function(index) {
-
-	// 	},
-	// 	removeByName: function(name) {
-
-	// 	}
-	// }
-
-
-	function Rate() {
+	function TimeLine(rate) {
 		this.timerId = 0
 		this.fnQueue = []
-		this.rate = 1
+		this.time = 0
+		this.rate = rate || 1
+		this.pid = null
 
 		this.current = __Date.now()
 		this.lastTime = __Date.now()
+		this.init()
 	}
 
-	Rate.prototype = {
+	TimeLine.prototype = {
 		addQueue: function(fn, delay, args, repeat) {
 			if (!fn) {
 				return
 			}
-			delay = (+delay < 1) ? 1 || +delay
+			delay = (+delay < 1) ? 1 : +delay
 			this.fnQueue[this.timerId] = {
 				fn: fn,
 				delay: delay,
 				args: args,
 				repeat: true,
 				sum: 0
-			}
+			};
 			return this.timerId++
 		},
 		delQueue: function(id) {
 			if (id < 0) {
 				return
 			}
+			console.log(this.time)
 			delete this.fnQueue[id]
 		},
-		timer: function() {
+		tickCycle: function() {
 			var cur = __Date.now()
 			var tickSpan = (cur - this.lastTime) * this.rate
 
@@ -120,19 +48,21 @@ window.Gear = (function(global, rate) {
 				if (task.repeat) {
 					var skip = (task.sum / task.delay) >> 0
 
-					if (skip > 20) {
-						skip = 20
-					}
+					// 最小粒度为1ms / 20，不要太过分
+					// if (skip > 30) {
+					// 	console.log('>>>')
+					// 	skip = 20
+					// }
 
 					while (--skip >= 0) {
-						excute(task)
+						this.excute(task)
 					}
 
 					// 剩余未执行的sum
 					task.sum %= task.delay
 				} else {
 					if (task.sum >= task.delay) {
-						excute(task)
+						this.excute(task)
 						delete this.fnQueue[k]
 						continue
 					}
@@ -142,21 +72,85 @@ window.Gear = (function(global, rate) {
 			this.current += tickSpan
 		},
 		excute: function(task) {
-			var code = task.code
+			this.time++
+			var code = task.fn
 			if (typeof code === 'function') {
 				code.apply(null, task.args || {})
 			}
 		},
 		init: function() {
+			var self = this
+			this.pid = global.setInterval(self.tickCycle.bind(self),100)
+		},
+		dispose: function() {
+			global.clearInterval(this.pid)
+			for (var k in this.fnQueue) {
+				this.fnQueue[k] = null
+			}
+			this.fnQueue = null
+		}
+	}
 
+	var slice = Function.call.bind(Array.prototype.slice)
+
+	var watch = function(target, prop, callback) {
+		Object.defineProperty(target, prop, {
+			get: function() {
+				return this['__' + prop]
+			},
+			set: function(val) {
+				if (val !== this['__' + prop]) {
+					this['__' + prop] = val
+					callback()
+				}
+			}
+		})
+	}
+
+	function TimeSpace(rate) {
+		this.rate = 1
+		this.timeline = new TimeLine()
+		this.init(rate)
+	}
+
+	TimeSpace.prototype = {
+		init: function(rate) {
+			this.setRate(rate)
+			this.timeline.rate = this.rate
+			this.watch()
+		},
+		setTimeout: function(fn, delay, args) {
+			args = args&&slice(args, 2)
+			return this.timeline.addQueue(fn, delay, args, false)
+		},
+		setInterval: function(fn, delay, args) {
+			args = args&&slice(args, 2)
+			return this.timeline.addQueue(fn, delay, args, true)
+		},
+		requestAnimationFrame: function(fn, args) {
+			this.setTimeout(fn, 16, args)
+		},
+		clearInterval: function(id) {
+			this.timeline.delQueue(id)
+		},
+		clearTimeout: function(id) {
+			this.timeline.delQueue(id)
+		},
+		watch: function() {
+			watch(this, 'rate', function() {
+				this.timeline.rate = this.rate
+			}.bind(this))
+		},
+		destroy: function() {
+
+		},
+		setRate: function (n) {
+			n = (n || 0) - 2 // n [0, 5]整数
+			this.rate = Math.pow(10, n)
 		}
 	}
 
 
-	// function slice([], args) {
-	// 	Array.prototype.slice.call([], args)
-	// }
-
-	var slice = Function.call.bind(Array.prototype.slice)
+	global.TimeSpace = TimeSpace	
 
 })(this)
